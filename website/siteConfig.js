1fe5c50591ac0e56ac694f22ba5bd5faacb2efb8
Jason Hsu <jhsu802701@jasonhsu.com>
2018-08-21 06:05:24 +0000

    Removed doc4

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -37,8 +37,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'}
+    {doc: 'doc1', label: 'Docs'}
   ],
 
   // If you have users set above, you add it here:
