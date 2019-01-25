ef9eaa1a986d1bae2bc0365ff702691e3ad59cb0
Jason Hsu <jhsu802701@jasonhsu.com>
2019-01-25 17:13:58 +0000

    Added links to examples in header and on index page

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -29,6 +29,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: '01-01', label: 'Docs'},
+    {doc: 'A-00', label: 'Examples'},
     {page: 'git', label: 'Git'},
     {page: 'help', label: 'Help'},
     {page: 'links', label: 'Links'}
